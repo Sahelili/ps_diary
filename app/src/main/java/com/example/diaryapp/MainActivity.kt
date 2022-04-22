8ec0cfb4bc98366e1cb45b2280e3b282522804f1
sahelili <sahelibanerjee99@gmail.com>
2022-04-22 11:42:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/diaryapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.diaryapp
+
+import android.icu.text.SimpleDateFormat
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navController= findNavController(R.id.fragmentContainerView)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp()|| super.onNavigateUp()
+    }
+}
