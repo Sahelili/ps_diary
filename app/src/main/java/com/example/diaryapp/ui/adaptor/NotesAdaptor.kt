f3523ac7570243e5005e2bd13d066cd4e9bdb302
pawan922000 <kumarpawan922000@gmail.com>
2022-06-11 14:10:03 +0530

    gache?

---
+++ app/src/main/java/com/example/diaryapp/ui/adaptor/NotesAdaptor.kt
@@ -0,0 +1,28 @@
+package com.example.diaryapp.ui.adaptor
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.diaryapp.databinding.ItemDiaryBinding
+import kotlinx.coroutines.NonDisposableHandle.parent
+import roommvvm.model.Notes
+
+class NotesAdaptor(val requireContext: Context,val notesList: List<Notes>) : RecyclerView.Adapter<NotesAdaptor.notesViewHolder>(){
+    class notesViewHolder(val binding: ItemDiaryBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): notesViewHolder {
+        return notesViewHolder(ItemDiaryBinding.inflate(
+            LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: notesViewHolder, position: Int) {
+        val data =notesList[position]
+        holder.binding.date.text=data.date
+        holder.binding.titleNotes.text=data.title
+
+    }
+
+    override fun getItemCount()= notesList.size
+}
--- app/src/main/java/com/example/diaryapp/ui/fragments/Homefragment.kt
+++ app/src/main/java/com/example/diaryapp/ui/fragments/Homefragment.kt
@@ -6,19 +6,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.diaryapp.R
 import com.example.diaryapp.databinding.FragmentHomefragmentBinding
+import com.example.diaryapp.ui.adaptor.NotesAdaptor
+import roommvvm.ViewModel.NotesViewModel
 
 class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentHomefragmentBinding
+    val viewModel: NotesViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding= FragmentHomefragmentBinding.inflate(layoutInflater,container,false)
+
+        viewModel.getNOtes().observe(viewLifecycleOwner,{ notesList->
+            binding.rcvAllNotes.layoutManager= GridLayoutManager(requireContext(),2)
+            binding.rcvAllNotes.adapter=NotesAdaptor(requireContext(),notesList)
+        })
 
         binding.btnAddNotes.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_homefragment_to_createNotesFragment)
