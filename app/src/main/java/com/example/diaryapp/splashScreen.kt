7a377d36e689e41690a6f90f954941a0dc9d0fcc
sahelili <sahelibanerjee99@gmail.com>
2022-06-16 23:26:27 +0530

    new update

---
+++ app/src/main/java/com/example/diaryapp/splashScreen.kt
@@ -0,0 +1,20 @@
+package com.example.diaryapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import kotlinx.coroutines.delay
+
+class splashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+                                       startActivity(Intent(this,MainActivity::class.java))
+        },delay(2000))
+    }
+}
