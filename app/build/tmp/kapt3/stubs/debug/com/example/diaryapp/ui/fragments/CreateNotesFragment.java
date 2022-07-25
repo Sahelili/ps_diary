918241de35e50477ec41b21d480e51cde1fd1cc2
pawan922000 <kumarpawan922000@gmail.com>
2022-07-25 18:03:16 +0530

    whole proj

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/diaryapp/ui/fragments/CreateNotesFragment.java
@@ -0,0 +1,40 @@
+package com.example.diaryapp.ui.fragments;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000<\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\u000f\u001a\u00020\u00102\b\u0010\u0011\u001a\u0004\u0018\u00010\u0012H\u0002J&\u0010\u0013\u001a\u0004\u0018\u00010\u00122\u0006\u0010\u0014\u001a\u00020\u00152\b\u0010\u0016\u001a\u0004\u0018\u00010\u00172\b\u0010\u0018\u001a\u0004\u0018\u00010\u0019H\u0016R\u001a\u0010\u0003\u001a\u00020\u0004X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\bR\u001b\u0010\t\u001a\u00020\n8FX\u0086\u0084\u0002\u00a2\u0006\f\n\u0004\b\r\u0010\u000e\u001a\u0004\b\u000b\u0010\f\u00a8\u0006\u001a"}, d2 = {"Lcom/example/diaryapp/ui/fragments/CreateNotesFragment;", "Landroidx/fragment/app/Fragment;", "()V", "binding", "Lcom/example/diaryapp/databinding/FragmentCreateNotesBinding;", "getBinding", "()Lcom/example/diaryapp/databinding/FragmentCreateNotesBinding;", "setBinding", "(Lcom/example/diaryapp/databinding/FragmentCreateNotesBinding;)V", "viewModel", "Lroommvvm/ViewModel/NotesViewModel;", "getViewModel", "()Lroommvvm/ViewModel/NotesViewModel;", "viewModel$delegate", "Lkotlin/Lazy;", "createNotes", "", "it", "Landroid/view/View;", "onCreateView", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
+public final class CreateNotesFragment extends androidx.fragment.app.Fragment {
+    public com.example.diaryapp.databinding.FragmentCreateNotesBinding binding;
+    @org.jetbrains.annotations.NotNull
+    private final kotlin.Lazy viewModel$delegate = null;
+    
+    public CreateNotesFragment() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    public final com.example.diaryapp.databinding.FragmentCreateNotesBinding getBinding() {
+        return null;
+    }
+    
+    public final void setBinding(@org.jetbrains.annotations.NotNull
+    com.example.diaryapp.databinding.FragmentCreateNotesBinding p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    public final roommvvm.ViewModel.NotesViewModel getViewModel() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable
+    @java.lang.Override
+    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull
+    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable
+    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable
+    android.os.Bundle savedInstanceState) {
+        return null;
+    }
+    
+    private final void createNotes(android.view.View it) {
+    }
+}
